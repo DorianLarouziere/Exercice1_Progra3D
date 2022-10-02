d1bead0a71a39cafebb7edf9ababb73f2bb64d27
DorianLarouziere <dorian.larouziere@student.junia.com>
2022-10-02 20:11:32 +0200

    Update InstantiatePrefabs.cs

--- Assets/Scripts/InstantiatePrefabs.cs
+++ Assets/Scripts/InstantiatePrefabs.cs
@@ -7,7 +7,7 @@ public class InstantiatePrefabs : MonoBehaviour
     [SerializeField] private uint numberOfPrefabs;
     [SerializeField] private float radiusOfCircle = 1;
     [SerializeField] private GameObject prefab;
-    [SerializeField] private Transform centerOfCircle;
+    [SerializeField] private Transform centerOfCircle = null;
     List<GameObject> generatedObjects = new List<GameObject>();
     private float x;
     private float y;
@@ -24,18 +24,32 @@ public class InstantiatePrefabs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 rotationToAdd = new Vector3(0, 0, 30) * Time.deltaTime;
-        centerOfCircle.Rotate(rotationToAdd);
+        if(centerOfCircle != null)
+        {
+            Vector3 rotationToAdd = new Vector3(0, 0, 30) * Time.deltaTime;
+            centerOfCircle.Rotate(rotationToAdd);
+        }
     }
 
     void InstantiateObjects()
     {
         for (int i = 0; i < numberOfPrefabs; i++)
         {
-            x = radiusOfCircle * Mathf.Sin(Mathf.PI * 2 * radiusForPrefab / 360) + centerOfCircle.position.x;
-            y = radiusOfCircle * Mathf.Cos(Mathf.PI * 2 * radiusForPrefab / 360) + centerOfCircle.position.y;
-            z = centerOfCircle.position.z;
-            generatedObjects.Add(Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity, centerOfCircle));
+            x = radiusOfCircle * Mathf.Sin(Mathf.PI * 2 * radiusForPrefab / 360);
+            y = radiusOfCircle * Mathf.Cos(Mathf.PI * 2 * radiusForPrefab / 360);
+            z = 0;
+            if(centerOfCircle != null)
+            {
+                x += centerOfCircle.position.x;
+                y += centerOfCircle.position.y;
+                z += centerOfCircle.position.z;
+                generatedObjects.Add(Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity, centerOfCircle));
+            }
+
+            else
+            {
+                generatedObjects.Add(Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity));
+            }
             radiusForPrefab += 360 / (float)numberOfPrefabs;
         }
     }
@@ -48,11 +62,9 @@ public class InstantiatePrefabs : MonoBehaviour
         }
     }
 
+    //Méthode appelée quand une variable est modifiée au niveau de l'inspector
     void OnValidate()
     {
-
-        /*if(generatedObjects.Count != 0)DestroyObjects();
-        */
         if(UnityEditor.EditorApplication.isPlaying)
         { 
             UnityEditor.EditorApplication.delayCall += () =>
@@ -62,10 +74,5 @@ public class InstantiatePrefabs : MonoBehaviour
 
             };
         }
-        
-        
-        //Invoke("InstantiateObjects",2f);
-
-        //Debug.Log("test");
     }
 }
